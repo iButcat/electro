3f5ed4ad7caa13edf147090c408c4feb051b162c
iButcat <Butcat@protonmail.com>
2020-08-31 13:50:03 +0200

    profile

--- electro_project/groups/models.py
+++ electro_project/groups/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from electro_social.models import UserInfo
+from electro_social.models import Profile
 
 
 class Group(models.Model):
