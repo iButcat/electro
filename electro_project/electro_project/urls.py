7f7b1fb0f2c9a250e902a3348e2f8703d2267eb7
iButcat <Butcat@protonmail.com>
2020-09-01 15:07:02 +0200

    change password + email config

--- electro_project/electro_project/urls.py
+++ electro_project/electro_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+import notifications.urls
 
 from . import views
 
@@ -14,8 +15,11 @@ urlpatterns = [
     path('thanks/', views.ThanksPage.as_view(), name='thanks'),
     path('posts/', include('posts.urls')),
     path('groups/', include('groups.urls')),
+    # redirect user detail
     path('user/<int:pk>', RedirectView.as_view(
     pattern_name='electro:detail', permanent=False), name='redirect'),
+    # notifications system
+    path('inbox/notifications/',include(notifications.urls, namespace='notifications')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- electro_project/posts/tests.py
+++ electro_project/posts/tests.py
@@ -1,1 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from .models import Post
+from electro_social.models import Profile
+
+class TestPostPages(SimpleTestCase):
+    def test_posts_list_page_status_code(self):
+        response = self.client.get('/posts/list')
+        self.assertEqual(response.status_code, 301)
