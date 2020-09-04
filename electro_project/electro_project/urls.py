9da9370a416c26b46366a482d828a5832eb5a9d5
iButcat <Butcat@protonmail.com>
2020-09-04 18:41:44 +0200

    rename function

--- electro_project/electro_project/urls.py
+++ electro_project/electro_project/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     # notifications system
     path('inbox/notifications/',include(notifications.urls, namespace='notifications')),
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
